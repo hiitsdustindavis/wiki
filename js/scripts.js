b117908944d1659d818e90c4509c920996d8ad91
Dustin Davis and Sean Burgoyne <student@epicodus.com>
2016-01-14 17:01:58 -0800

    Add html pages for concepts

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,11 @@ $(document).ready(function() {
 
     // Declaring our variable array for our list of topics "var = topics"//
 
-    var topics = ["Markdown", "Block elements", "Inline elements", "CSS", "Bootstrap", "Divs and spans", "Floats", "Box model", "C scading styles", "Grid system", "Responsive design", "JavaScript (JS) arithmetic", "JS variables", "JS methods", "JS strings", "JS functions", "jQuery", "DOM manipulation and traversal", "Forms", "Attributes", "Branching", "Arrays", "Looping"];
+    var topics = ["Markdown", "Block-elements", "Inline-elements", "CSS", "Bootstrap", "Divs-and-spans", "Floats", "Box-model", "C-scading-styles", "Grid-system", "Responsive-design", "JavaScript-JS-arithmetic", "JS-variables", "JS-methods", "JS-strings", "JS-functions", "jQuery", "DOM-manipulation-and-traversal", "Forms", "Attributes", "Branching", "Arrays", "Looping"];
 
     // Set up a For loop / set index to zero /  set condition to run length of array /adding one until condition is met//
     var concept = $("select.concept").val();
-    
+
 
   for (var index = 0; index <= concept; index += 1) {
       location.href = './pages/' + topics[index] + '.html';
